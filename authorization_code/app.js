f8e5bfc093510ccf4e9f8e8ce9b9b2c7e090593d
Christian Pinto <chrpinto@sas.upenn.edu>
2021-01-15 20:13:05 -0500

    Added user playlist and tracks feature

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -14,7 +14,7 @@ var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
 var client_id = '4d1a43614a814305935e18c9796db331'; // Your client id
-var client_secret = ''; // Your secret
+var client_secret = '4d64ed0397f641229f991b01a89bbe2b'; // Your secret
 var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
 /**
@@ -46,7 +46,7 @@ app.get('/login', function(req, res) {
   res.cookie(stateKey, state);
 
   // your application requests authorization
-  var scope = 'user-read-private user-read-email';
+  var scope = 'user-read-private user-read-email user-top-read playlist-read-private';
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
